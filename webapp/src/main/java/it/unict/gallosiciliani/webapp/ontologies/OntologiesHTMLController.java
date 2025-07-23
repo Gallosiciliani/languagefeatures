85d8e3c50077e0ac557f1028a70716e1fc9f323a
Cristiano Longo <cristianolongo@opendatahacklab.org>
2025-07-23 15:58:29 +0200

    Additional link to download ontologies in ttl #57

--- webapp/src/main/java/it/unict/gallosiciliani/webapp/ontologies/OntologiesHTMLController.java
+++ webapp/src/main/java/it/unict/gallosiciliani/webapp/ontologies/OntologiesHTMLController.java
@@ -29,7 +29,7 @@ public class OntologiesHTMLController {
     @Autowired
     Projects projects;
 
-    @GetMapping("liph")
+    @GetMapping(value = "liph", params = "!ttl")
     String getLiph(final Model model){
         model.addAttribute("title", liph.getName());
         model.addAttribute("comment", liph.getComment());
@@ -47,14 +47,15 @@ public class OntologiesHTMLController {
 //        return items.stream().map((i)->{return new HashedOntologyInternalItem(i, ontologyNS);}).toList();
     }
 
-    @GetMapping("gs-features")
+    @GetMapping(value = "gs-features", params = "!ttl")
     String getGsFeatures(final Model model){
         model.addAttribute("title", gsFeatures.getName());
+        model.addAttribute("comment", gsFeatures.getComment());
         model.addAttribute("phenomena", gsFeatures.getRegexLinguisticPhenomena());
         return "ontologies/viewGSFeatures.html";
     }
 
-    @GetMapping(value={"projects", "projects/", "projects/", "projects/gallosiciliani2023Project"})
+    @GetMapping(value={"projects", "projects/", "projects/", "projects/gallosiciliani2023Project"}, params="!ttl")
     String getGallosiciliani2023Project(final Model model){
         model.addAttribute("project", projects.getGallosiciliani2023Project());
         //we need the following to sort the project results
--- webapp/src/test/java/it/unict/gallosiciliani/webapp/ontologies/OntologiesRestControllerTest.java
+++ webapp/src/test/java/it/unict/gallosiciliani/webapp/ontologies/OntologiesRestControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -40,21 +41,22 @@ public class OntologiesRestControllerTest {
 
     @Test
     void shouldReturnLiphOntologyTTL() throws Exception {
-        shouldReturnOntologyInTTLFormat("/ns/liph", liph);
-        shouldReturnOntologyInTTLFormat("/ns/liph/ttl", liph);
+        //shouldReturnOntology("/ns/liph", liph);
+        shouldReturnOntology("/ns/liph?ttl", liph, false);
     }
 
     @Test
     void shouldReturnGSFeaturesOntologyTTL() throws Exception {
-        shouldReturnOntologyInTTLFormat("/ns/gs-features", gsFeatures);
-        shouldReturnOntologyInTTLFormat("/ns/gs-features/ttl", gsFeatures);
+        shouldReturnOntology("/ns/gs-features", gsFeatures);
+        shouldReturnOntology("/ns/gs-features?ttl", gsFeatures, false);
     }
 
     @Test
     void shouldReturnProjects() throws Exception {
-        shouldReturnOntologyInTTLFormat("/ns/projects", projects);
-        shouldReturnOntologyInTTLFormat("/ns/projects/", projects);
-        shouldReturnOntologyInTTLFormat("/ns/projects/gallosiciliani2023Project", projects);
+        shouldReturnOntology("/ns/projects", projects);
+        shouldReturnOntology("/ns/projects/", projects);
+        shouldReturnOntology("/ns/projects?ttl", projects, false);
+        shouldReturnOntology("/ns/projects/gallosiciliani2023Project", projects);
     }
 
     @Test
@@ -62,14 +64,18 @@ public class OntologiesRestControllerTest {
         shouldReturnAboxTTL("/ns/lexica");
         shouldReturnAboxTTL("/ns/lexica/");
         shouldReturnAboxTTL("/ns/lexica/nicosiaesperlinga");
-        shouldReturnAboxTTL("/ns/lexica/nicosiaesperlinga/ttl");
+        shouldReturnAboxTTL("/ns/lexica/nicosiaesperlinga?ttl", false);
     }
 
     private void shouldReturnAboxTTL(final String path) throws Exception {
+        shouldReturnAboxTTL(path, true);
+    }
+
+        private void shouldReturnAboxTTL(final String path, final boolean requestTTL) throws Exception {
         final String expectedFile="test";
         when(abox.getOntologyAsStr()).thenReturn(expectedFile);
-        mockMvc.perform(get(path)
-                        .accept("text/turtle")).andExpect(status().isOk())
+        mockMvc.perform(requestTTL ? get(path)
+                        .accept("text/turtle"): get(path)).andExpect(status().isOk())
                 .andExpect(content().contentType("text/turtle;charset=UTF-8"))
                 .andExpect(content().string(expectedFile));
     }
@@ -79,12 +85,23 @@ public class OntologiesRestControllerTest {
      * @param path relative path for the request
      * @param o the mock for the ontology loader
      */
-    private void shouldReturnOntologyInTTLFormat(final String path, final OntologyLoader o) throws Exception {
+    private void shouldReturnOntology(final String path, final OntologyLoader o) throws Exception {
+        shouldReturnOntology(path, o, true);
+    }
+
+    /**
+     * Test that the ontology is returned as a raw file
+     * @param path relative path for the request
+     * @param o the mock for the ontology loader
+     * @param ttl set ttl as requested mime-type
+     */
+    private void shouldReturnOntology(final String path, final OntologyLoader o, final boolean ttl) throws Exception {
         final String expectedFile="test";
         when(o.getOntologyAsStr()).thenReturn(expectedFile);
-        mockMvc.perform(get(path)
-                        .accept("text/turtle")).andExpect(status().isOk())
+        final MockHttpServletRequestBuilder get=get(path);
+        mockMvc.perform(ttl ? get.accept("text/turtle") : get).andExpect(status().isOk())
                 .andExpect(content().contentType("text/turtle;charset=UTF-8"))
                 .andExpect(content().string(expectedFile));
     }
+
 }
