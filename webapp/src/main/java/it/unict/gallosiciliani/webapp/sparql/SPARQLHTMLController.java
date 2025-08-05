4aafce4808eba3071349b3f48e0f9658d227955a
Cristiano Longo <cristianolongo@opendatahacklab.org>
2025-08-05 11:41:28 +0200

    Add metadata to the lexicon individual #65  and Disable sparql endpoint #64

--- webapp/src/main/java/it/unict/gallosiciliani/webapp/sparql/SPARQLHTMLController.java
+++ webapp/src/main/java/it/unict/gallosiciliani/webapp/sparql/SPARQLHTMLController.java
@@ -1,5 +1,6 @@
 package it.unict.gallosiciliani.webapp.sparql;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Web pages for querying the knowledge base with sparql
  */
 @Controller
+@ConditionalOnExpression("${it.unict.gallosiciliani.sparql:false}")
 @RequestMapping("/sparql")
 public class SPARQLHTMLController {
 
